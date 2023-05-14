a72e97ca40eda4ccd2cd4f662022c25db732e94b
lucifer <mjrskyboy@yahoo.com>
2023-05-14 11:12:11 +0330

    Third release of the exercise

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function shuffle(array) {
-    array.sort(() => Math.random() - 0.5);
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
 }
 
 arr = [1, 2, 3, 4, 5, 6];
